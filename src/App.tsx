be29fca44b3ad0f91df926881fbb232a856ac36c
Edron1 <edronmcposht@gmail.com>
2023-08-20 16:21:15 +0300

    First

---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import React, { useState, ChangeEvent, useEffect } from 'react';
+import './App.css';
+import Weather from './components/Weather';
+import { Select, Modal, Button, Input, Card } from 'antd';
+
+function App() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [city, setCity] = useState<string>('');
+  const [selectCity, setSelectCity] = useState();
+  const [data, setData] = useState<any[]>([]);
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  function success(pos: any) {
+    var crd = pos.coords;
+    localStorage.setItem('selectCity', crd);
+    let temp = localStorage.getItem('selectCity');
+  }
+
+  function errors(err: any) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  useEffect(() => {
+    let temp = JSON.parse(localStorage.getItem('selectCity') || '{}');
+    setSelectCity(temp);
+    // if(!!temp) {
+    //   setSelectCity(JSON.parse(temp));
+    // }
+    // setSelectCity(temp); 
+    if (navigator.geolocation && localStorage.getItem('selectCity') === null) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            //If granted then you can directly call your function here
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "prompt") {
+            //If prompt then the user will be asked to give permission
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+        });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+    getWeather(city);
+  };
+  let getWeather = async (value: string) => {
+        const api_url = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&language=Russian`);
+        const data = await api_url.json();
+        setData(data.results);
+  }
+  return (
+    <div className="App">
+      <h1>Прогноз погоды</h1>
+      <Button type="primary" onClick={showModal}>
+        Выбрать город
+      </Button>
+
+      <Modal title="Выбрать город" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Input type="text" value={city} onChange={handleChange}/>
+        <div>
+          {data?.map(item => {
+            return <Card 
+              onClick={() => {
+                setSelectCity(item); 
+                localStorage.setItem('selectCity', JSON.stringify(item));
+              }} 
+              hoverable 
+              size='small' 
+              style={{marginTop: 10}}>
+              <div style={{display: 'inline'}}>
+                <span>{item.country}</span> 
+                <span style={{marginLeft: 5}}>{item.name}</span>
+              </div>
+              <p>{item.admin1}</p>
+            </Card>
+          }
+            
+          )}
+        </div>
+      </Modal>
+
+      <Weather city={selectCity}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.tsx
@@ -0,0 +1,162 @@
+import React, {useState, useEffect} from 'react';
+import { Card, Collapse } from 'antd';
+import wind from '../img/icons/wind.png';
+import gauge from '../img/icons/gauge.png';
+import humidity from '../img/icons/humidity.png';
+import cloud from '../img/icons/cloud.png';
+import uvindex from '../img/icons/uv-index.png';
+
+function Weather(city: any) {
+
+  const [data, setData] = useState();
+  const [temp] = useState([0, 1, 2, 3, 4, 5, 6]);
+  const [temp2] = useState({
+    0: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+    1: [24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47],
+    2: [48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71],
+    3: [72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95],
+    4: [96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119],
+    5: [120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143],
+    6: [144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167],
+  });
+  const [dayIndex, setDayIndex] = useState<number>(0);
+  const [dailyWeather, setDailyWeather] = useState({
+    precipitation_sum: [],
+    shortwave_radiation_sum: [],
+    temperature_2m_max: [],
+    temperature_2m_min: [],
+    time: [],
+    weathercode: [],
+    windspeed_10m_max: []
+  });
+  const [hourlyWeather, setHourlyWeather] = useState({
+    apparent_temperature: [],
+    cloudcover: [],
+    direct_radiation: [],
+    precipitation: [],
+    pressure_msl: [],
+    relativehumidity_2m: [],
+    temperature_2m: [],
+    time: [],
+    weathercode: [],
+    windspeed_10m: []
+  });
+  const [current_weather, setCurrent] = useState({
+    is_day: '',
+    temperature: '',
+    time: '',
+    weathercode: '',
+    winddirection: '',
+    windspeed: ''
+  });
+  const [dailyUnits, setDailyUnits] = useState({
+    precipitation_sum: '',
+    shortwave_radiation_sum: '',
+    temperature_2m_max: '',
+    temperature_2m_min: '',
+    time: '',
+    weathercode: '',
+    windspeed_10m_max: ''
+  });
+  const [hourlyUnits, setHourlyUnits] = useState({
+    apparent_temperature: '',
+    cloudcover: '',
+    direct_radiation: '',
+    precipitation: '',
+    pressure_msl: '',
+    relativehumidity_2m: '',
+    temperature_2m: '',
+    time: '',
+    weathercode: '',
+    windspeed_10m: ''
+  })
+
+  let getWeather = async () => {
+    const api_url = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${city.city?.latitude}&longitude=${city.city?.longitude}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,pressure_msl,precipitation,cloudcover,direct_radiation,windspeed_10m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max,shortwave_radiation_sum&timezone=${city.city?.timezone}&current_weather=true
+    `);
+    const data = await api_url.json();
+    setData(data);
+    setCurrent(data.current_weather);
+    setDailyWeather(data.daily);
+    setDailyUnits(data.daily_units);
+    setHourlyWeather(data.hourly);
+    setHourlyUnits(data.hourly_units);
+    localStorage.setItem('currentWeather', JSON.stringify(data.current_weather));
+}
+
+  useEffect(() => {
+    getWeather();
+  }, [city]);
+
+  return (
+    <div>
+      {current_weather ? <Collapse
+      defaultActiveKey={['1']}
+      style={{marginTop: 30}}
+      size="large"
+      items={[{ key: '1', label: 'Погода сейчас', children: <div>
+      <div style={{width: 250, marginLeft: 'auto', marginRight: 'auto'}}>
+        <h2>Погода сейчас</h2>
+        <Card className='Weather-Card' title={current_weather?.time}>
+          <span><img src={uvindex} className='icons'/> {current_weather?.temperature} {dailyUnits?.temperature_2m_max}</span>
+          <span><br/><img src={cloud} className='icons'/> Код погоды - {current_weather?.weathercode} {dailyUnits?.weathercode}</span>
+          <span><br/><img src={wind} className='icons'/> Направление ветра - {current_weather?.winddirection}</span>
+          <span><br/><img src={wind} className='icons'/> Скорость ветра - {current_weather?.windspeed} {dailyUnits?.windspeed_10m_max}</span>
+        </Card>
+           
+        
+      </div>
+    </div> }]}
+    />: ''}
+
+    {dailyWeather ? <Collapse
+      defaultActiveKey={['2']}
+      size="large"
+      items={[{ key: '2', label: 'Прогноз погоды по дням', children: <div style={{marginLeft: 'auto', marginRight: 'auto'}}>
+      <h2>Прогноз погоды по дням</h2>
+      <div style={{display: 'flex', flex: '1', flexWrap: 'wrap', justifyContent: 'center'}}>
+          {temp.map(index => {
+            return <div style={{minWidth: 200, margin: 5}}>
+              <Card hoverable className='Weather-Card' onClick={() => setDayIndex(index)} title={dailyWeather?.time[index]}>        
+                <span>Сумма осадков - {dailyWeather?.precipitation_sum[index]} {dailyUnits?.precipitation_sum}</span>
+                <span><br/>Коротковолновое излучение - {dailyWeather?.shortwave_radiation_sum[index]} {dailyUnits?.shortwave_radiation_sum}</span>
+                <span><br/><img src={uvindex} className='icons'/> Макс. температура - {dailyWeather?.temperature_2m_max[index]} {dailyUnits?.temperature_2m_max}</span>
+                <span><br/><img src={uvindex} className='icons'/> Мин. температура - {dailyWeather?.temperature_2m_min[index]} {dailyUnits?.temperature_2m_min}</span>
+                <span><br/><img src={cloud} className='icons'/> Код погоды - {dailyWeather?.weathercode[index]} {dailyUnits?.weathercode}</span>
+                <span><br/> <img src={wind} className='icons'/> Макс. скорость ветра - {dailyWeather?.windspeed_10m_max[index]} {dailyUnits?.windspeed_10m_max}</span>
+              </Card>
+            </div>
+          })}
+        </div>
+    </div> }]}
+    />: ''}
+
+    {hourlyWeather ? <Collapse
+      defaultActiveKey={['3']}
+      size="large"
+      items={[{ key: '3', label: 'Прогноз погоды по часам', children: <div style={{marginLeft: 'auto', marginRight: 'auto'}}>
+      <h2>Прогноз погоды по часам</h2>
+      <div style={{display: 'flex', flex: '1', flexWrap: 'wrap', justifyContent: 'center'}}>
+      {temp2[dayIndex as keyof typeof temp2].map(index => {
+        return <div style={{minWidth: 200, margin: 5}}>
+          <Card className='Weather-Card' title={hourlyWeather?.time[index]}>        
+            <span><img src={uvindex} className='icons'/> Ощущаемая температура - {hourlyWeather?.apparent_temperature[index]} {hourlyUnits?.apparent_temperature}</span>
+            <span><br/><img src={cloud} className='icons'/> Облачность - {hourlyWeather?.cloudcover[index]} {hourlyUnits?.cloudcover}</span>
+            <span><br/>Прямое излучение - {hourlyWeather?.direct_radiation[index]} {hourlyUnits?.direct_radiation}</span>
+            <span><br/>Осадки - {hourlyWeather?.precipitation[index]} {hourlyUnits?.precipitation}</span>
+            <span><br/><img src={gauge} className='icons'/> Давление - {hourlyWeather?.pressure_msl[index]} {hourlyUnits?.pressure_msl}</span>
+            <span><br/><img src={humidity} className='icons'/> Относительная влажность - {hourlyWeather?.relativehumidity_2m[index]} {hourlyUnits?.relativehumidity_2m}</span>
+            <span><br/><img src={uvindex} className='icons'/> Температура - {hourlyWeather?.temperature_2m[index]} {hourlyUnits?.temperature_2m}</span>
+            <span><br/><img src={cloud} className='icons'/> Код погоды - {hourlyWeather?.weathercode[index]} {hourlyUnits?.weathercode}</span>
+            <span><br/><img src={wind} className='icons'/> Скорость ветра - {hourlyWeather?.windspeed_10m[index]} {hourlyUnits?.windspeed_10m}</span>
+          </Card>
+        </div>
+      })}
+    </div>
+  </div> }]}
+    />: ''}
+    </div>
+  );
+}
+
+export default Weather;
